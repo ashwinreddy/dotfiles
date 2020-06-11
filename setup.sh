d23f67eb71d2426128f034abb894304ec38bddd5
Ashwin Reddy <adreddy@berkeley.edu>
2020-06-11 00:45:54 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script runs the commands needed to make the rest of the repo work on a clean machine
+
+tmux source-file ~/.tmux.conf
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
